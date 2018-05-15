86c7969c4cef9047f833cb61026c0ccfd8fb6121
zhuangdm <zhuangdm@asiainfo.com>
2018-05-15 17:24:10 +0800

    leetcode daily

---
+++ 283_move_zeros.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i = 0; i < n-1; i++) {
+            if(nums[i] == 0) {
+                int j = i + 1;
+                while(j < n) {
+                    if(nums[j] != 0)
+                        break;
+                    j++;
+                }
+                if(j >= n)
+                    break;
+                nums[i] = nums[i] + nums[j];
+                nums[j] = nums[i] - nums[j];
+                nums[i] = nums[i] - nums[j];
+            }
+        }
+    }
+};
